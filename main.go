c2e587585c3f770a67ab411e59e00016688f4394
Benjamin Fourmaux <49273769+BenjaminFourmaux@users.noreply.github.com>
2026-04-11 15:49:37 +0200

    adding swagger with swaggo

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"os"
 	"quay-go-api/api"
+	_ "quay-go-api/docs"
 	"quay-go-api/service/logger"
 )
 
+// @title Quay Go API
+// @version 1.0
+// @description Quay registry API implemented in Go
 func main() {
 	if logLevel := os.Getenv("LOG_LEVEL"); logLevel != "" {
 		logger.SetLevel(logger.StringToLevel(logLevel))
